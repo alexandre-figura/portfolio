e5e3298d1e7dc3d06b802e22931b746fe586832a
Alexandre Figura <git@alexandre.figura.live>
2015-06-06 19:44:55 +0200

    Remove the logger from the manager.
    Because the logger was not used.

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
-import logging
-
 from flask_script import Manager, Server
 
 from portfolio import helpers
 from portfolio.portfolio import app
 
-logger = logging.getLogger(__name__)
 manager = Manager(app)
 
 
@@ -16,10 +13,5 @@ class RunServer(Server):
         super().__call__(app, *args, **kwargs)
 
 if __name__ == '__main__':
-    console = logging.StreamHandler()
-    console.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
-    logger.addHandler(console)
-    logger.setLevel(logging.INFO)
-
     manager.add_command('runserver', RunServer(use_debugger=True))
     manager.run()
