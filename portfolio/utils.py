deb6f85a24329d1ad8825f81e89f2b2c1c6703ac
Alexandre Figura <git@alexandre.figura.live>
2015-06-13 20:17:48 +0200

    Update default behavior of Jinja2.
    By default, Jinja2 evaluates undefined variables to empty strings. This can lead to web pages rendered with partial content. This behavior has been modified to raise an exception instead.

--- portfolio/utils.py
+++ portfolio/utils.py
@@ -1,6 +1,8 @@
 import subprocess
 
 from flask import Flask
+import jinja2
+from werkzeug.datastructures import ImmutableDict
 
 from . import pages
 
@@ -15,6 +17,10 @@ def create_app(**kwargs):
     app.config.from_object('portfolio.default_settings')
     app.config.from_pyfile('portfolio.cfg', silent=True)
     app.config.update(kwargs)
+
+    # In order to not partially render content of web pages,
+    # the template engine must raise an exception for undefined variables.
+    app.jinja_options = ImmutableDict(undefined=jinja2.StrictUndefined, **app.jinja_options)
 
     # Initialize extensions.
     pages.init_app(app)
--- tests/system_and_unit_tests/test_utils.py
+++ tests/system_and_unit_tests/test_utils.py
@@ -1,20 +1,29 @@
 import subprocess
 
+import flask
+import jinja2
 import pytest
 
 import portfolio
 from portfolio import utils
 
 
-def test_application_factory():
-    app = utils.create_app(CONFIG_KEY_1='key1', CONFIG_KEY_2='key2')
+class TestApplicationFactory:
+    def test_application_is_configured_accordingly_to_keyword_parameters(self):
+        app = utils.create_app(CONFIG_KEY_1='key1', CONFIG_KEY_2='key2')
+        assert app.config['CONFIG_KEY_1'] == 'key1'
+        assert app.config['CONFIG_KEY_2'] == 'key2'
 
-    # Extensions must be initialized.
-    assert portfolio.pages.app is app
+    def test_extensions_are_initialized(self):
+        app = utils.create_app()
+        assert portfolio.pages.app is app
 
-    # Custom configuration must be handled.
-    assert app.config['CONFIG_KEY_1'] == 'key1'
-    assert app.config['CONFIG_KEY_2'] == 'key2'
+    def test_jinja2_is_configured_to_not_silently_pass_undefined_variables(self):
+        app = utils.create_app()
+
+        with pytest.raises(jinja2.exceptions.UndefinedError):
+            with app.test_request_context():
+                flask.render_template_string('{{ undefined_variable }}')
 
 
 class TestWatchSassStylesheets:
