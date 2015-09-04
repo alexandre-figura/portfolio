ab2ce5c07601a507003120e20e17c339b84c928b
Alexandre Figura <git@alexandre.figura.live>
2015-09-04 09:47:01 +0200

    Update test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
+import os.path
+
 import pytest
 
-from portfolio.utils import create_app
+from portfolio.helpers import create_app
 
 
 @pytest.fixture
 def app():
-    instance = create_app(TESTING=True)
+    tests_dir = os.path.dirname(os.path.abspath(__file__))
+    pages_dir = os.path.sep.join((tests_dir, '/pages'))
+
+    instance = create_app(
+        TESTING=True,
+        FLATPAGES_ROOT=pages_dir,
+    )
     return instance
